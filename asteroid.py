e43e7ed2080da517fb2b6e4f4574ec1adcc2dc00
finnesserr <callumrhysjones@outlook.com>
2025-09-11 20:08:46 +0100

    Complete Base Game

---
+++ asteroid.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+from circleshape import *
+from constants import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, ("white"), (int(self.position.x), int(self.position.y)), self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            angle = random.uniform(20, 50)
+
+            sm_asteroid_1 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+            sm_asteroid_2 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
+
+            sm_asteroid_1.velocity = self.velocity.rotate(angle) * 1.2
+            sm_asteroid_2.velocity = self.velocity.rotate(-angle) * 1.2
+
+            sm_asteroid_1.add(*Asteroid.containers)
+            sm_asteroid_2.add(*Asteroid.containers)
--- main.py
+++ main.py
@@ -1,39 +1,72 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+# Import necessary modules
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from shot import Shot
 
 def main():
    
     # Initialize Pygame
-   
     pygame.init()
    
     # Create the game window
-   
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    # Create a clock & delta time object to manage the frame rate
-   
+    # Create a clock & delta time object to manage the frame rate 
     clock = pygame.time.Clock()
     dt = 0
 
+    # Create Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    # Assign instances to groups
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+    # Create Player instance
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    # Create AsteroidField instance
+    asteroid_field = AsteroidField()
+    
     # Main game loop
     # This loop will run until the user closes the window
-    # or the game ends
-    
+    # or the game ends 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        # Clear the screen with black
         screen.fill("black")
-        pygame.display.flip()
-       
         # Limit the frame rate to 60 FPS and get delta time in seconds
-       
-        dt = clock.tick(60) / 1000  
+        dt = clock.tick(60) / 1000
+        # Update and draw game objects here
+        for shape in drawable:
+            shape.draw(screen)
+        updatable.update(dt)
+        
+        # Check for collisions between player and asteroids
+        for asteroid in asteroids:
+            if player.collide(asteroid):
+                raise sys.exit("Game Over!")
+        # Check for collisions between shots and asteroids
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collide(shot):
+                    asteroid.split()
+                    shot.kill()
+
+
+        # Update the display
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
