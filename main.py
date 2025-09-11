083af6f60b2a48838b2c266211a245bc2ad457b5
Finnesserr <callumrhysjones@outlook.com>
2025-09-11 16:38:56 +0100

    Template with screen window v2

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
     # Create the game window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -17,11 +14,12 @@ def main():
     # This loop will run until the user closes the window
     # or the game ends
     while True:
-        pygame.Surface.fill(screen, (0, 0, 0))  # Fill the screen with black
-        pygame.display.flip()  # Update the display
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
