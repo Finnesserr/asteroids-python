83bbf815dd6c4db026ac0eb44b7593aec58d5220
Finnesserr <callumrhysjones@outlook.com>
2025-09-11 16:59:28 +0100

    FPS

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ import pygame
 from constants import *
 
 def main():
+   
+    # Initialize Pygame
+   
     pygame.init()
-
+   
     # Create the game window
+   
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Create a clock & delta time object to manage the frame rate
+   
+    clock = pygame.time.Clock()
+    dt = 0
 
     # Main game loop
     # This loop will run until the user closes the window
     # or the game ends
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,6 +30,10 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+       
+        # Limit the frame rate to 60 FPS and get delta time in seconds
+       
+        dt = clock.tick(60) / 1000  
 
 if __name__ == "__main__":
     main()
